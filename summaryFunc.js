7150d1de4475d1f0fa91cc9e4551f361f15869ad
MakarFrontend <152717190+MakarFrontend@users.noreply.github.com>
2024-02-11 21:28:08 +0300

    11.02.2024 Дозагрузил,

--- summaryFunc.js
+++ summaryFunc.js
@@ -1,6 +1,7 @@
 "use strict";
 let analitic = false;
 let setting = false;
+let howPlay = false;
 function summaryFunc(elem) {
     if (elem == 'analitic') {
         if (analitic == false) {
@@ -17,6 +18,14 @@ function summaryFunc(elem) {
         } else if (setting == true) {
             document.querySelector('#listSummarySetting').outerHTML = `<img id="listSummarySetting" src="toClose.png" style="height: 15px; width: 15px;">`;
             setting = false;
+        }
+    } else if (elem == 'howPlay') {
+        if (howPlay == false) {
+            document.querySelector('#listSummaryhowPlay').outerHTML = `<img id="listSummaryhowPlay" src="toTop.png" style="height: 15px; width: 15px;">`;
+            howPlay = true;
+        } else if (howPlay == true) {
+            document.querySelector('#listSummaryhowPlay').outerHTML = `<img id="listSummaryhowPlay" src="toClose.png" style="height: 15px; width: 15px;">`;
+            howPlay = false;
         }
     }
 }
